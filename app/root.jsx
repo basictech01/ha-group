82f0de40e91e4af2f23490402417d865c0a91d9a
basictech01 <prapandey@linkedin.com>
2025-03-16 18:35:06 +0530

    fix: font

--- app/root.jsx
+++ app/root.jsx
@@ -11,8 +11,12 @@ export function Layout({ children }) {
   return (
     <html lang="en">
       <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lora:ital,wght@0,400..700;1,400..700&family=Secular+One&display=swap" rel="stylesheet" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
         <Meta />
         <Links />
         <style>{`
